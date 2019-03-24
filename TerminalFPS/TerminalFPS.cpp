4da9b44d936488bd2b1f337e8eb16b69fe635d28
DiomChen <153583852@qq.com>
2019-03-24 23:10:23 +0800

    完成句柄

--- TerminalFPS/TerminalFPS.cpp
+++ TerminalFPS/TerminalFPS.cpp
@@ -1,10 +1,20 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+
 int main()
 {
+	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE,0,NULL,CONSOLE_TEXTMODE_BUFFER,NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwByteWriten = 0;
 	return 0;
+
 }
 
